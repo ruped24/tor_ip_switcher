04a414c1f7085e874b8027d90c686b2105dcb117
Rupert Edwards <12942791+ruped24@users.noreply.github.com>
2018-08-22 11:09:45 -0400

    Configuration setup script for /etc/tor/torrc

--- tips_setup.py
+++ tips_setup.py
@@ -16,45 +16,43 @@ ControlHashedPassword = ''.join(
 
 
 def controlPort_setup():
-    system("sed -i '/ControlPort /s/^#//' /etc/tor/torrc")
-    system("sed -i '/HashedControlPassword /s/^#//' /etc/tor/torrc")
+  system("sed -i '/ControlPort /s/^#//' /etc/tor/torrc")
+  system("sed -i '/HashedControlPassword /s/^#//' /etc/tor/torrc")
 
 
 def controlHashed_password():
-    system(
-        "sed -i 's/^HashedControlPassword 16:.*[A-Z0-9]/HashedControlPassword %s/' /etc/tor/torrc"
-        % ControlHashedPassword)
+  system(
+      "sed -i 's/^HashedControlPassword 16:.*[A-Z0-9]/HashedControlPassword %s/' /etc/tor/torrc"
+      % ControlHashedPassword)
 
 
 def reload_tor_config():
-    tor_running = getoutput('pidof tor')
-    if tor_running:
-        system('kill -HUP $(pidof tor)')
-        print("\n \033[92m[" + u'\u2714' + "]\033[0m Tor config: Reloaded")
-    else:
-        print(str())
+  tor_running = getoutput('pidof tor')
+  if tor_running:
+    system('kill -HUP $(pidof tor)')
+    print("\n \033[92m[" + u'\u2714' + "]\033[0m Tor config: Reloaded")
+  else:
+    print(str())
 
 
 if __name__ == '__main__':
-    if argv[1:] == list():
-        exit(" \n[!] Usage: %s <your_new_password>\n" % basename(__file__))
-    else:
-        try:
-            if isfile('/etc/tor/torrc'):
-                controlPort_setup()
-                controlHashed_password()
-                reload_tor_config()
-                print(" \033[92m[" + u'\u2714' +
-                      "]\033[0m ControlPort: Enabled\n", "\033[92m[" +
-                      u'\u2714' + "]\033[0m ControlHashedPassword: Enabled\n",
-                      "\033[92m[" + u'\u2714' +
-                      "]\033[0m /etc/tor/torrc updated successfully!\n",
-                      "\033[92m[" + u'\u2719' +
-                      "]\033[0m Password set to: %s\n" % ''.join(argv[1:]),
-                      "\033[92m[" + u'\u2719' +
-                      "]\033[0m ControlHashedPassword: %s\n" %
-                      ControlHashedPassword)
-            else:
-                exit("\033[91m[!]\033[0m /etc/tor/torrc missing.")
-        except Exception as err:
-            print(err)
+  if argv[1:] == list():
+    exit(" \n[!] Usage: %s <your_new_password>\n" % basename(__file__))
+  else:
+    try:
+      if isfile('/etc/tor/torrc'):
+        controlPort_setup()
+        controlHashed_password()
+        reload_tor_config()
+        print(" \033[92m[" + u'\u2714' + "]\033[0m ControlPort: Enabled\n",
+              "\033[92m[" + u'\u2714' +
+              "]\033[0m ControlHashedPassword: Enabled\n", "\033[92m[" +
+              u'\u2714' + "]\033[0m /etc/tor/torrc updated successfully!\n",
+              "\033[92m[" + u'\u2719' +
+              "]\033[0m Password set to: %s\n" % ''.join(argv[1:]),
+              "\033[92m[" + u'\u2719' +
+              "]\033[0m ControlHashedPassword: %s\n" % ControlHashedPassword)
+      else:
+        exit("\033[91m[!]\033[0m /etc/tor/torrc missing.")
+    except Exception as err:
+      print(err)
